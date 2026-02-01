221f3c92c528f34ae267337b9246f3d387ac5a59
Aanchal Singh <aanchalsingh569@gmail.com>
2026-02-01 21:37:53 +0530

    fix: remove invalid wildcard route and fix CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,23 @@ app.use(
   })
 );
 
-app.options("*", cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 
 app.use(express.json());
 
